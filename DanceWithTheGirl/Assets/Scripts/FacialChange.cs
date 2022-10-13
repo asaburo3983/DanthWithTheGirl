9bb1f820295d0789279913b1dee9b3132f23cf2a
asaburo3983 <54845686+asaburo3983@users.noreply.github.com>
2022-10-14 00:26:10 +0900

    表情変化テスト

---
+++ DanceWithTheGirl/Assets/Scripts/FacialChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacialChange : MonoBehaviour
+{
+
+    public SkinnedMeshRenderer faceMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //表情変化テスト
+        var index = faceMesh.sharedMesh.GetBlendShapeIndex("Fcl_BRW_Angry");
+        faceMesh.SetBlendShapeWeight(index, 100);
+        index = faceMesh.sharedMesh.GetBlendShapeIndex("Fcl_BRW_Angry");
+        faceMesh.SetBlendShapeWeight(index, 100);
+        index = faceMesh.sharedMesh.GetBlendShapeIndex("Fcl_MTH_Angry");
+        faceMesh.SetBlendShapeWeight(index, 100);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMesh.SetBlendShapeWeight(2, 100);
+        int a = 100;
+    }
+}
