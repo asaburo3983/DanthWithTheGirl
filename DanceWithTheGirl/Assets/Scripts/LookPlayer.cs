99fad008cc596ccfee5f85f481de5c16452f4292
snena712 <57130699+snena712@users.noreply.github.com>
2022-11-14 13:38:03 +0900

    カメラのほうへ向く処理

---
+++ DanceWithTheGirl/Assets/Scripts/LookPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookPlayer : MonoBehaviour
+{
+    private Animator avator;
+    public Transform lookAtObj = null;
+
+    [SerializeField, Range(0, 1)]
+    private float lookAtWeight = 1.0f;
+    [SerializeField, Range(0, 1)]
+    private float bodyWeight = 0.4f;
+    [SerializeField, Range(0, 1)]
+    private float headWeight = 0.7f;
+    [SerializeField, Range(0, 1)]
+    private float eyesWeight = 0.5f;
+    [SerializeField, Range(0, 1)]
+    private float clampWeight = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        avator = GetComponent<Animator>();
+        if (lookAtObj == null)
+        {
+            lookAtObj = Camera.main.transform;
+        }
+    }
+
+    void OnAnimatorIK(int layorIndex)
+    {
+        if (avator)
+        {
+            avator.SetLookAtWeight(lookAtWeight, bodyWeight, headWeight, eyesWeight, clampWeight);
+            avator.SetLookAtPosition(lookAtObj.position);
+        }
+    }
+}
