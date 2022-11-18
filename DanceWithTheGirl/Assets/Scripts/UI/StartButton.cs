e83de3ea3b736d9e74453cfe4723f33ca8f473f0
situki <58246408+situki@users.noreply.github.com>
2022-11-18 12:04:28 +0900

    Update StartButton.cs

--- DanceWithTheGirl/Assets/Scripts/UI/StartButton.cs
+++ DanceWithTheGirl/Assets/Scripts/UI/StartButton.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class StartButton : MonoBehaviour
 {
     [SerializeField] float time = 1.0f;
@@ -25,21 +28,72 @@ public class StartButton : MonoBehaviour
         }
     }
 
-    void FadeIn()
+    public void FadeIn()
     {
-
         if (TryGetComponent<Image>(out var im)) {
             Color col = im.color;
             col.a = 0;
 
-            im.DOColor(col, time).OnComplete(() => Destroy(this.gameObject));
+            im.DOColor(col, time).OnComplete(() => {
+                col.a = 0;
+                im.color = col;
+            });
         }
 
         if(TryGetComponent<Text>(out var tx)) {
             Color col = tx.color;
             col.a = 0;
 
-            tx.DOColor(col, time).OnComplete(() => Destroy(this.gameObject));
+            im.DOColor(col, time).OnComplete(() => {
+                col.a = 0;
+                im.color = col;
+            });
+        }
+    }
+
+    public void FadeOut()
+    {
+        if (TryGetComponent<Image>(out var im)) {
+            Color col = im.color;
+            col.a = 1;
+
+            im.DOColor(col, time).OnComplete(() => {
+                col.a = 1;
+                im.color = col;
+            });
+        }
+
+        if (TryGetComponent<Text>(out var tx)) {
+            Color col = tx.color;
+            col.a = 1;
+
+            tx.DOColor(col, time).OnComplete(() => {
+                col.a = 1;
+                im.color = col;
+            });
         }
     }
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(StartButton))]
+public class startButtonEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("フェードイン")) {
+            ((StartButton)target).FadeIn();
+        }
+        if (GUILayout.Button("フェードあうと")) {
+            ((StartButton)target).FadeOut();
+        }
+    }
+
+}
+
+
+#endif
